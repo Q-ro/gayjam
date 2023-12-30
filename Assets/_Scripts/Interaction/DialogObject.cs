7c78809b36f18708ec3b1ce894d304b3cedf860e
Q-ro <andres.mrad@gmail.com>
2023-12-30 15:02:59 -0500

    NPC Object Spawner

--- Assets/_Scripts/Interaction/DialogObject.cs
+++ Assets/_Scripts/Interaction/DialogObject.cs
@@ -6,6 +6,8 @@ namespace Scripts.Interaction
 {
     public class DialogObject : InteractableObjectBase
     {
+        [SerializeField] GameObject objectToSpawn;
+
         [Header("Ink JSON")]
         [SerializeField] private TextAsset inkJson;
         [SerializeField] private GameObject bubbleGameObject;
@@ -19,35 +21,44 @@ namespace Scripts.Interaction
             InputManager.OnInteract += EndDialogue;
         }
 
-        private void OnDestroy(){
+        private void OnDestroy()
+        {
             InputManager.OnInteract -= EndDialogue;
         }
         public override void Interact()
         {
-            
+
         }
 
         public override void StartInteraction()
         {
-            if(!dialogueManager.CanExitDialogue && !dialogueManager.DialogueIsPlaying){
+            if (!dialogueManager.CanExitDialogue && !dialogueManager.DialogueIsPlaying)
+            {
                 dialogueManager.DialogueBubble = bubbleGameObject;
                 dialogueManager.DialogueText = bubbleText;
                 Debug.Log("Entering Dialogue Mode");
                 dialogueManager.EnterDialogueMode(inkJson);
             }
-            
+
         }
 
         public override void EndInteraction()
         {
-           
+
         }
 
-        private void EndDialogue(){
-             if(dialogueManager.CanExitDialogue){
+        private void EndDialogue()
+        {
+            if (dialogueManager.CanExitDialogue)
+            {
                 Debug.Log("Exit dialog");
                 dialogueManager.CanExitDialogue = false;
                 IsInteractable = false;
+                if (objectToSpawn == null)
+                    return;
+                var spawnPosition = GameObject.FindWithTag("TableObjectSpawnerPosition").transform.position;
+                var go = Instantiate(objectToSpawn);
+                go.transform.position = spawnPosition;
             }
         }
     }
